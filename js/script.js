1b37bbba9d89c6cda90d2f4bcbd3edb1171043c9
Habibur Rahman <41167924+habibur899@users.noreply.github.com>
2018-12-08 22:00:57 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	// Slick Slider Active
+	$('.slider-active').slick({
+		autoplay:true,
+		prevArrow: '<i class="fas fa-angle-left"></i>',
+		nextArrow: '<i class="fas fa-angle-right"></i>'
+	});
+});
